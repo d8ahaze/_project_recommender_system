397530667b20274d41bf6c5f3356aa77493607c3
Ann <nitsuku@mail.ru>
2021-10-22 19:38:02 +0300

    changing the csv file

--- venv/main.py
+++ venv/main.py
@@ -1,34 +1,11 @@
 import csv
 
-name_1 = "da"
-name_2 = "ta"
-data_names = ["ma", "in"]
-
 with open("data.csv", "w") as file:
     writer = csv.writer(file)
     writer.writerow(
-        #[name_1, name_2]
-        #чтобы открыть в виде таблицы можно использовать LibreOffice
-        #data_names
-        ("user_name", "user_rename")
+        ("id_news", "id_user", " tags", "url",)
+
     )
-user_data = [
-    ["user1", "rename1"],
-    ["user1", "rename1"],
-    ["user1", "rename1"],
-]
-
-for user in user_data:
-    with open("data.csv", "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            user
-        )
 
 
-#объединение данных в одну коллекцию
-# with open("data.csv", "a") as file:
-#        writer = csv.writer(file)
-#        writer.writerows(
-#            user_data
-#        )
+
